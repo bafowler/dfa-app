385eeb9888d646697026df2ed10d27caca855574
Brennan Fowler <bafowler98@gmail.com>
2021-05-24 13:23:16 -0400

    Fade error message in and then out

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,23 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Text } from 'react-konva';
 
 const ERROR_OFFSET = 15;
 
 export default function ErrorMessage({ duration, message, position, removeError }) {
+  const errorRef = useRef(null);
+  const [ fadeIn, setFadeIn ] = useState(true);
+
   useEffect(() => {
-    setTimeout(() => {
-      removeError();
-    }, duration);
-  }, [ duration, message, removeError ])
+    const durationInSeconds = duration / 1000;
+
+    if (errorRef.current) {
+      if (fadeIn) {
+        errorRef.current.to({
+          opacity: 1,
+          duration: durationInSeconds / 2,
+        });
+        setTimeout(() => setFadeIn(false), duration / 2);
+      } else {
+        errorRef.current.to({
+          opacity: 0,
+          duration: durationInSeconds / 2,
+        });
+        setTimeout(() => removeError(), duration / 2);
+      }
+    }
+  }, [ duration, fadeIn, removeError ]);
 
   return (
     <Text
+      ref={errorRef}
       x={position.x + ERROR_OFFSET}
       y={position.y + ERROR_OFFSET}
       fontFamily='Gothic A1'
       fontSize={14}
       fill='red'
       text={message}
+      opacity={0}
     />
   );
 }
