46df5de24163619e4bf436e2a61495166c2aa8d3
bafowler <bafowler98@gmail.com>
2021-05-29 11:47:02 -0400

    Change implementation of curved arrows to custom parabola

--- src/NodeArrow.js
+++ src/NodeArrow.js
@@ -1,52 +1,27 @@
 import { Arrow, Line } from 'react-konva';
 import NodeArrowText from './NodeArrowText';
-import CurvedLine from './CurvedLine';
+import { getPerpendicularVector, getPointsOnCurve, getUnitVector } from './utils';
 
-const NODE_ARROW_SPACE = 16;
+const NODE_TEXT_SPACE = 16;
 const CURVE_MULTIPLIER = 40;
 
-const getLinePoints = (initialPosition, midpoint, unitVector) => {
-  const lineStart = [ initialPosition.x, initialPosition.y ];
-  const lineEnd = [ midpoint.x - (unitVector.x * NODE_ARROW_SPACE), midpoint.y - (unitVector.y * NODE_ARROW_SPACE) ];
-  return [ ...lineStart, ...lineEnd ];
-};
-
-const getArrowPoints = (midpoint, currentPosition, unitVector) => {
-  const arrowStart = [ midpoint.x + (unitVector.x * NODE_ARROW_SPACE), midpoint.y + (unitVector.y * NODE_ARROW_SPACE) ];
-  const arrowEnd = [ currentPosition.x, currentPosition.y ];
-  return [ ...arrowStart, ...arrowEnd ];
-};
-
-const getBezierControlPoint = (start, end) => {
-  const midpoint = {
-    x: (end.x + start.x) / 2,
-    y: (end.y + start.y) / 2
-  };
-  const length = Math.hypot(end.x - start.x, end.y - start.y);
-  const unitVector = {
-    x: (end.x - start.x) / length,
-    y: (end.y - start.y) / length,
-  }
-  const perpendicularUnitVector = {
-    x: unitVector.y,
-    y: -unitVector.x
-  }
-  return {
-    x: midpoint.x + (perpendicularUnitVector.x * 20),
-    y: midpoint.y + (perpendicularUnitVector.y * 20),
-  }
-};
+const getLineEndAndArrowStart = (textPosition, unitVector, space) => (
+  [{ 
+    x: textPosition.x - (unitVector.x * space), 
+    y: textPosition.y - (unitVector.y * space) 
+  }, { 
+    x: textPosition.x + (unitVector.x * space), 
+    y: textPosition.y + (unitVector.y * space) 
+  }]
+);
 
 export default function NodeArrow({ initialPosition, currentPosition, incomplete=false, curved=false, removeArrow, addError }) {
   const midpoint = {
     x: (currentPosition.x + initialPosition.x) / 2, 
     y: (currentPosition.y + initialPosition.y) / 2
   };
-  const length = Math.hypot(currentPosition.x - initialPosition.x, currentPosition.y - initialPosition.y);
-  const unitVector = {
-    x: (currentPosition.x - initialPosition.x) / length,
-    y: (currentPosition.y - initialPosition.y) / length
-  };
+  const unitVector = getUnitVector(initialPosition, currentPosition);
+  const perpendicularUnitVector = getPerpendicularVector(unitVector);
 
   if (incomplete) {
     return (
@@ -56,52 +31,29 @@ export default function NodeArrow({ initialPosition, currentPosition, incomplete
         stroke='black' 
       />
     );
-  } else if (curved) {
-    const perpendicularUnitVector = {
-      x: unitVector.y,
-      y: -unitVector.x,
-    };
-    const apex = {
-      x: midpoint.x + (perpendicularUnitVector.x * CURVE_MULTIPLIER),
-      y: midpoint.y + (perpendicularUnitVector.y * CURVE_MULTIPLIER)
-    };
-   const firstLineEnd = { 
-     x: apex.x - (unitVector.x * NODE_ARROW_SPACE), 
-     y: apex.y - (unitVector.y * NODE_ARROW_SPACE) 
-    };
-   const secondLineStart = { 
-     x: apex.x + (unitVector.x * NODE_ARROW_SPACE), 
-     y: apex.y + (unitVector.y * NODE_ARROW_SPACE) 
-    };
-
-
-    return (
-      <> 
-        <CurvedLine 
-          points={[ initialPosition, firstLineEnd ]} 
-          control={getBezierControlPoint(initialPosition, firstLineEnd)}
-        /> 
-        <NodeArrowText position={apex} removeArrow={removeArrow} addError={addError} />
-        <CurvedLine 
-          points={[ secondLineStart, currentPosition ]} 
-          control={getBezierControlPoint(secondLineStart, currentPosition)}
-        /> 
-      </>
-    );
   } else {
+    let linePoints, lineEnd, arrowPoints, arrowStart, textPosition;
+
+    if (curved) {
+      textPosition = {
+        x: midpoint.x + (perpendicularUnitVector.x * CURVE_MULTIPLIER),
+        y: midpoint.y + (perpendicularUnitVector.y * CURVE_MULTIPLIER)
+      }; 
+      [ lineEnd, arrowStart ] = getLineEndAndArrowStart(textPosition, unitVector, NODE_TEXT_SPACE);
+      linePoints = getPointsOnCurve(initialPosition, currentPosition, textPosition, initialPosition, lineEnd);
+      arrowPoints = getPointsOnCurve(initialPosition, currentPosition, textPosition, arrowStart, currentPosition);
+    } else {
+      textPosition = midpoint;
+      [ lineEnd, arrowStart ] = getLineEndAndArrowStart(textPosition, unitVector, NODE_TEXT_SPACE);
+      linePoints = [ initialPosition.x, initialPosition.y, lineEnd.x, lineEnd.y ];
+      arrowPoints = [ arrowStart.x, arrowStart.y, currentPosition.x, currentPosition.y ];
+    }
+
     return (
       <>
-        <Line 
-          points={getLinePoints(initialPosition, midpoint, unitVector)}
-          fill='black'
-          stroke='black'
-        />
-        <NodeArrowText position={midpoint} removeArrow={removeArrow} addError={addError} />
-        <Arrow 
-          points={getArrowPoints(midpoint, currentPosition, unitVector)} 
-          fill='black' 
-          stroke='black' 
-        />
+        <Line points={linePoints} fill='black' stroke='black' />
+        <NodeArrowText position={textPosition} removeArrow={removeArrow} addError={addError} />
+        <Arrow points={arrowPoints} fill='black' stroke='black' />
       </>
     );
   }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const X_AXIS_UNIT_VECTOR = { x: 1, y: 0 };
+
 // Return true iff dest is within the circle defined by center and radius
 export const withinCircle = (center, dest, radius) => (Math.pow(dest.x - center.x, 2) + Math.pow(dest.y - center.y, 2)) < Math.pow(radius, 2);
 
@@ -15,4 +17,77 @@ export const isArrowBetweenNodes = (arrows, startNodeId, endNodeId) => {
     return false;
   }
   return !!arrows.find(arrow => arrow.startNodeId === startNodeId && arrow.endNodeId === endNodeId);
-}
+};
+
+// Return the vector of length 1 connecting points p1 and p2
+export const getUnitVector = (p1, p2) => {
+  const length = Math.hypot(p2.x - p1.x, p2.y - p1.y);
+  return {
+    x: (p2.x - p1.x) / length,
+    y: (p2.y - p1.y) / length
+  };
+};
+
+// Return the vector perpdendicular to v
+export const getPerpendicularVector = v => ({
+  x: -v.y,
+  y: v.x
+});
+
+// Return the angle between l1 and l2 in radians
+export const getAngleBetweenLines = (l1, l2) => {
+  const length1 = Math.hypot(l1.x, l1.y);
+  const length2 = Math.hypot(l2.x, l2.y);
+  let angle = Math.acos((l1.x * l2.x) + (l1.y * l2.y) / (length1 * length2));
+  if (l2.y <= l1.y) {
+    return angle;
+  } else {
+    return (Math.PI * 2) - angle;
+  }
+};
+
+// Given a point in the standard axis, return the corresponding
+// point in the axis rotated by angle radians
+export const getStandardPointInRotatedAxis = (point, angle) => ({
+  x: point.x * Math.cos(angle) - point.y * Math.sin(angle),
+  y: point.y * Math.cos(angle) + point.x * Math.sin(angle)
+});
+
+// Given a point in the axis rotated by angle radians, return
+// the corresponding point in the standard axis
+export const getRotatedPointInStandardAxis = (point, angle) => ({
+  x: point.x * Math.cos(angle) + point.y * Math.sin(angle),
+  y: point.y * Math.cos(angle) - point.x * Math.sin(angle)
+});
+
+// Return the parabolic function defined by vertex and point
+export const getParabola = (point, vertex) => {
+  const a = (point.y - vertex.y) / Math.pow(point.x - vertex.x, 2);
+  return x => a * Math.pow(x - vertex.x, 2) + vertex.y;
+};
+
+// Return an array of points between pointsStart and pointsEnd 
+// on the parabola defined by curveStart, curveEnd, and curveVertex
+// where curveStart and curveEnd define a new axis of rotation
+export const getPointsOnCurve = (curveStart, curveEnd, curveVertex, pointsStart, pointsEnd) => {
+  const angle = getAngleBetweenLines(X_AXIS_UNIT_VECTOR, getUnitVector(curveStart, curveEnd));
+  const rotatedCurveStart = getStandardPointInRotatedAxis(curveStart, angle);
+  const rotatedCurveVertex = getStandardPointInRotatedAxis(curveVertex, angle);
+  const f = getParabola(rotatedCurveStart, rotatedCurveVertex);
+
+  const rotatedStart = getStandardPointInRotatedAxis(pointsStart, angle);
+  const rotatedEnd = getStandardPointInRotatedAxis(pointsEnd, angle);
+  const numPoints = Math.floor(rotatedEnd.x - rotatedStart.x);
+  const points = [];
+
+  for (let i = 0; i < numPoints; i++) {
+    const point = getRotatedPointInStandardAxis({
+      x: rotatedStart.x + i,
+      y: f(rotatedStart.x + i),
+    }, angle);
+
+    points.push(point.x);
+    points.push(point.y);
+  }
+  return points;
+};
